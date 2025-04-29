b250c35297953346f577584441470ef4cbcfed6b
Rémi <remilabonne@yahoo.fr>
2025-04-29 10:03:02 +0200

    J'ai résolu les conflits

--- morpion.py
+++ morpion.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 def lit_grille(fichier):
     grille = {'A': ['.', '.', '.'], 'B': ['.', '.', '.'], 'C': ['.', '.', '.']}
     with open(fichier, 'r') as f:
         ch = f.read()
         ch = ch.strip()
         i = 0
-        print(ch)
+       # print(ch)
         while i < len(ch):
             lettre = ch[i]
             i += 1
@@ -13,7 +12,7 @@ def lit_grille(fichier):
             i += 1
             joueur = ch[i]
             grille[lettre][int(chiffre) - 1] = joueur
-            i += 1
+            i += 1  
     return grille  # <-- return en dehors de la boucle !
 
 def afficher_grille(grille: dict):
@@ -22,12 +21,3 @@ def afficher_grille(grille: dict):
         print(' | '+' | '.join(l)+' | ')
         print(' ' + '-'*13)
 afficher_grille(lit_grille('grille.morpion'))
-=======
-grille ={'A':['','' ,''], 'B':['','' ,''], 'C':['','' ,'']}
-
-def afficher_grille(grille: dict):
-    for ligne, valeur in grille.items():
-        print (f"{ligne} | {valeur}")
-
-afficher_grille(grille)
->>>>>>> a58883a7620755511f001ddfec53d1477b98c5eb
